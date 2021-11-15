4cf2db36cc00aa06f577afc4ac198d558b338a30
Rhoda Evangelene <rhoda.evangelene@gmail.com>
2021-11-15 17:26:26 -0500

    created form and form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,16 @@
   
   buttonEl.addEventListener("click", createTaskHandler);
   
+  var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
